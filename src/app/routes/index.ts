174b30f25387b2591cc5f234fe7bb61d5bff4049
Emon Hossain <islamemon59@gmail.com>
2026-02-09 16:56:42 +0600

    better user schema enum auth added

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,7 +1,10 @@
 import { Router } from "express";
 import { specialtyRouter } from "../module/specialty/specialty.routes";
+import { authRoutes } from "../module/auth/auth.routes";
 
 const router = Router()
+
+router.use("/auth", authRoutes);
 
 router.use("/specialties", specialtyRouter);
 
