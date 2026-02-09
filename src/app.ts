1d85f1df8abd54d95ed17eb0f96337f366959bf7
Emon Hossain <islamemon59@gmail.com>
2026-02-09 10:39:58 +0600

    prisma installed table crated data added

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,17 @@
 import express, { Application, Request, Response } from "express";
+import { prisma } from "./app/lib/prisma";
 const app: Application = express();
 
+app.get("/", async (req: Request, res: Response) => {
+  const result = await prisma.specialty.create({
+    data: {
+      title: "Cardiology",
+    },
+  });
 
-app.get("/", (req: Request, res: Response) => {
+  res
+    .status(201)
+    .json({ success: true, message: "Specialty created", data: result });
   res.send("Hello, World!");
 });
 
