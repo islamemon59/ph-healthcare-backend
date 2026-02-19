d8945ad82d36a226fc4e3af3ba1f6143bd77d61d
Emon Hossain <islamemon59@gmail.com>
2026-02-19 22:19:44 +0600

    bug fixed

---
+++ src/app/utils/QueryBuilder.ts
@@ -0,0 +1,44 @@
+import { unknown } from "better-auth";
+import {
+  IQueryConfig,
+  IQueryParams,
+  PrismaCountAge,
+  PrismaCountArgs,
+  PrismaFindManyArgs,
+  PrismaModelDelegate,
+} from "../interfaces/query.interface";
+
+export class QueryBuilder<
+  T,
+  TWhereInput = Record<string, unknown>,
+  TInclude = Record<string, unknown>,
+> {
+  private query: PrismaFindManyArgs;
+  private countQuery: PrismaCountArgs;
+  private page: number = 1;
+  private limit: number = 10;
+  private skip: number = 0;
+  private sortBy: string = "createdAt";
+  private sortOrder: "asc" | "desc" = "desc";
+  private selectFields: Record<string, boolean> | unknown;
+
+  constructor(
+    private model: PrismaModelDelegate,
+    private queryParams: IQueryParams,
+    private config: IQueryConfig,
+  ) {
+    this.query = {
+      where: {},
+      include: {},
+      orderBy: {},
+      skip: 0,
+      take: 10,
+    };
+
+    this.countQuery = {
+      where: {},
+    };
+
+
+  }
+}
--- src/config/cloudinary.config.ts
+++ src/config/cloudinary.config.ts
@@ -1,4 +1,4 @@
-import { v2 as cloudinary } from "cloudinary";
+import { v2 as cloudinary, UploadApiResponse } from "cloudinary";
 import { envVars } from "./env";
 import AppError from "../app/errorHelpers/AppError";
 import status from "http-status";
@@ -8,6 +8,59 @@ cloudinary.config({
   api_key: envVars.CLOUDINARY.CLOUDINARY_API_KEY,
   api_secret: envVars.CLOUDINARY.CLOUDINARY_API_SECRET,
 });
+
+export const uploadFileToCloudinary = async (
+  buffer: Buffer,
+  fileName: string,
+): Promise<UploadApiResponse> => {
+  if (!buffer || !fileName) {
+    throw new AppError(
+      status.BAD_REQUEST,
+      "File buffer or file name is missing",
+    );
+  }
+
+  const extension = fileName.split(".").pop()?.toLocaleLowerCase();
+  const fileNameWithoutExtension = fileName
+    .split(".")
+    .slice(0, -1)
+    .join(".")
+    .toLocaleLowerCase()
+    .replace(/\s+/g, "-")
+    .replace(/[^a-zA-Z0-9-]/g, "");
+
+  const uniqueName =
+    Math.random().toString(36).substring(2) +
+    "-" +
+    Date.now() +
+    "-" +
+    fileNameWithoutExtension;
+
+  const folder = extension === "pdf" ? "pdfs" : "images";
+
+  return await new Promise((resolve, reject) => {
+    cloudinary.uploader
+      .upload_stream(
+        {
+          resource_type: "auto",
+          folder: `ph-healthcare/${folder}/${uniqueName}`,
+          public_id: uniqueName,
+        },
+        (error, result) => {
+          if (error) {
+            reject(
+              new AppError(
+                status.INTERNAL_SERVER_ERROR,
+                "Error uploading file to cloudinary",
+              ),
+            );
+          }
+          resolve(result as UploadApiResponse);
+        },
+      )
+      .end(buffer);
+  });
+};
 
 export const deleteFileFromCloudinary = async (url: string) => {
   try {
